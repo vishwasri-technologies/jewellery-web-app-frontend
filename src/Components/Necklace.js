6bb9a9fb7f4edf852a621a20a3bab8814c078c7b
ashwith26 <ashwith26@gmail.com>
2025-02-07 17:19:22 +0530

    Added images to Necklace page

--- src/Components/Necklace.js
+++ src/Components/Necklace.js
@@ -1,121 +1,140 @@
 import React from "react";
 import "./Necklace.css";
-import image from "../assets/image-222.png"; 
+import image1 from "../assets/necklace/image-1.png"; 
+import image2 from "../assets/necklace/image-2.png";
+import image3 from "../assets/necklace/image-3.png";
+import image4 from "../assets/necklace/image-4.png";
+import image5 from "../assets/necklace/image-5.png";
+import image6 from "../assets/necklace/image-6.png";
+import image7 from "../assets/necklace/image-7.png";
+import image8 from "../assets/necklace/image-8.png";
+import image9 from "../assets/necklace/image-9.png";
+import image10 from "../assets/necklace/image-10.png";
+import image11 from "../assets/necklace/image-11.png";
+import image12 from "../assets/necklace/image-12.png";
+import image13 from "../assets/necklace/image-13.png";
+import image14 from "../assets/necklace/image-14.png";
+import image15 from "../assets/necklace/image-15.png";
+import image16 from "../assets/necklace/image-16.png";
+import image17 from "../assets/necklace/image-17.png";
+import image18 from "../assets/necklace/image-18.png"; 
+import Footer from "./Footer";
 
 const products = [
   {
     id: 1,
-    name: "Gold Necklace",
-    price: "$200",
-    image: image,
+    name: "Choker with Earrings",
+    price: "₹1400",
+    image: image1,
   },
   {
     id: 2,
-    name: "Diamond Ring",
-    price: "$500",
-    image: image,
+    name: "Temple Nacklace Set",
+    price: "₹1400",
+    image: image2,
   },
   {
     id: 3,
-    name: "Silver Bracelet",
-    price: "$150",
-    image: image,
+    name: "Stone-Studded Choker",
+    price: "₹1400",
+    image: image3,
   },
   {
     id: 4,
-    name: "Pearl Earrings",
-    price: "$120",
-    image: image,
+    name: "Cubic Zirconia Nacklace ",
+    price: "₹1400",
+    image: image4,
   },
   {
     id: 5,
-    name: "Platinum Pendant",
-    price: "$300",
-    image: image,
+    name: "Gold & Pearl necklace set",
+    price: "₹1400",
+    image: image5,
   },
   {
     id: 6,
-    name: "Ruby Ring",
-    price: "$450",
-    image: image,
+    name: "Polki Choker",
+    price: "₹1400",
+    image: image6,
   },
   {
     id: 6,
-    name: "Ruby Ring",
-    price: "$450",
-    image: image,
+    name: "Antique Nacklace",
+    price: "₹1400",
+    image: image7,
   },
   {
     id: 6,
-    name: "Ruby Ring",
-    price: "$450",
-    image: image,
+    name: "Red Kundan Necklace ",
+    price: "₹1400",
+    image: image8,
   },
   {
     id: 6,
-    name: "Ruby Ring",
-    price: "$450",
-    image: image,
+    name: "Pearl Necklace Set",
+    price: "₹1400",
+    image: image9,
   },
   {
     id: 6,
-    name: "Ruby Ring",
-    price: "$450",
-    image: image,
+    name: "Nacklaces Set",
+    price: "₹1400",
+    image: image10,
   },
   {
     id: 6,
-    name: "Ruby Ring",
-    price: "$450",
-    image: image,
+    name: "Sofine Oxidised Plated Mirror Necklace Set",
+    price: "₹1400",
+    image: image11,
   },
   {
     id: 6,
-    name: "Ruby Ring",
-    price: "$450",
-    image: image,
+    name: "Long Nacklace Set",
+    price: "₹1400",
+    image: image12,
   },
   {
     id: 6,
-    name: "Ruby Ring",
-    price: "$450",
-    image: image,
+    name: "Haram",
+    price: "₹1400",
+    image: image13,
   },
   {
     id: 6,
-    name: "Ruby Ring",
-    price: "$450",
-    image: image,
+    name: "Fancy Jewellery Set ",
+    price: "₹1400",
+    image: image14,
   },
   {
     id: 6,
-    name: "Ruby Ring",
-    price: "$450",
-    image: image,
+    name: "Lakshmi & Peacock Motifs Nacklace Set",
+    price: "₹1400",
+    image: image15,
   },
   {
     id: 6,
-    name: "Ruby Ring",
-    price: "$450",
-    image: image,
+    name: "Temple Haram",
+    price: "₹1400",
+    image: image16,
   },
   {
     id: 6,
-    name: "Ruby Ring",
-    price: "$450",
-    image: image,
+    name: "Lakshmi Nacklace Set",
+    price: "₹1400",
+    image: image17,
   },
   {
     id: 6,
-    name: "Ruby Ring",
-    price: "$450",
-    image: image,
+    name: "Lakshmi Haram",
+    price: "₹1400",
+    image: image18,
   },
   
 ];
 
 const Necklace = () => {
   return (
+    <div>
     <div className="container">
       <div className="grid-container">
         {products.map((product) => (
@@ -132,6 +151,8 @@ const Necklace = () => {
           </div>
         ))}
       </div>
+    </div>
+    <Footer />
     </div>
   );
 };
