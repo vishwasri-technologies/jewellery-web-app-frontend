481d022a5df405953866cc37ba02182779d026b5
Chinthala naveen <Chinthalanaveen504@gmail.com>
2025-02-08 11:17:25 +0530

    added images of Earrings

--- src/Components/EarRings.js
+++ src/Components/EarRings.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+
 const EarringsRings = () => {
-  return <h1 style={styles.page}>Create an account to explore more! Ear rings</h1>;
-};
+  return
+  <>
 
-const styles = { page: { textAlign: "center", marginTop: "50px", fontSize: "24px", color: "purple" } };
 
+
+  </>
+}
 export default EarringsRings;
