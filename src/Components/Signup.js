9b4a4a0045396ad7d0d3df95d1bed8437eecdfe1
Chinthala naveen <Chinthalanaveen504@gmail.com>
2025-02-10 11:40:40 +0530

    added signup page

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,8 +1,8 @@
 import React from "react";
-// import "./Signup.css";
+
+import "./Signup.css";
 import { Link } from "react-router-dom"; 
-// import google from "../assets/google-icon.png";
-// import facebook from "../assets/facebook-icon.png";
+
 
 const Signup = () => {
   return (
@@ -10,23 +10,29 @@ const Signup = () => {
       <div className="signup-form1">
         <h2>Sign Up</h2>
         <form>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" placeholder="" />
 
-          <label htmlFor="phone">Phone no</label>
-          <input type="text" id="phone" placeholder="" />
+          <label htmlFor="name" >Name</label>
+          <input type="text" id="name" placeholder=""  className="signup-label"/>
+
+          <label htmlFor="phone" >Phone no</label>
+          <input type="text" id="phone" placeholder="" className="signup-label" />
 
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" placeholder="" />
+          <input type="email" id="email" placeholder="" className="signup-label"/>
 
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" placeholder="" />
-            <div className="reset-password-container1">
+          <label htmlFor="password" >Password</label>
+          <input type="password" id="password" placeholder="" className="signup-label"/>
+            {/* <div className="reset-password-container1">
+\
               <input type="checkbox" id="reset-checkbox" />
               <label htmlFor="reset-checkbox" className="reset-password1">
                 Reset&nbsp;&nbsp;Password
               </label>
+<<<<<<< HEAD
             </div>
+=======
+            </div> */}
+
 
           <button type="submit" className="signup-button">
             Sign Up
