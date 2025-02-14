89bb728fb17dfb44d013c9341a3809d373e22dae
PrashanthiNallapu <prashanthinallapu2002@gmail.com>
2025-02-14 11:42:25 +0530

    updated and removed navbar in some components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Navbar from "./Components/Navbar";
+// import Home from "./Components/Home";
+// import Necklace from "./Components/Necklace";
+// import Chains from "./Components/Chains";
+// import All from "./Components/All.js";
+// import EarRings from "./Components/EarRings.js";
+// import Bangles from "./Components/Bangles.js";
+// import Signup from "./Components/Signup.js";
+// import Logout from "./Components/Logout.js";
+// import Login from "./Components/Login.js";
+// import Reset from "./Components/Reset.js";
+
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Signup />} />
+//         <Route path="/all" element={<All />} />
+//         <Route path="/necklace" element={<Necklace />} />
+//         <Route path="/chains" element={<Chains />} />
+//         <Route path="/earrings" element={<EarRings />} />
+//         <Route path="/bangles" element={<Bangles />} />
+//         <Route path="/signup" element={<Signup />} />
+//         <Route path="/logout" element={<Logout />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/Reset" element={<Reset />} />
+//         <Route path="/home" element={<Home />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Necklace from "./Components/Necklace";
@@ -12,23 +54,34 @@ import Logout from "./Components/Logout.js";
 import Login from "./Components/Login.js";
 import Reset from "./Components/Reset.js";
 
+const MainLayout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
+);
 
 const App = () => {
   return (
     <Router>
-      <Navbar />
       <Routes>
+        {/* Routes with Navbar */}
+        <Route element={<MainLayout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/all" element={<All />} />
+          <Route path="/necklace" element={<Necklace />} />
+          <Route path="/chains" element={<Chains />} />
+          <Route path="/earrings" element={<EarRings />} />
+          <Route path="/bangles" element={<Bangles />} />
+      
+        </Route>
+
+        {/* Routes without Navbar */}
         <Route path="/" element={<Signup />} />
-        <Route path="/all" element={<All />} />
-        <Route path="/necklace" element={<Necklace />} />
-        <Route path="/chains" element={<Chains />} />
-        <Route path="/earrings" element={<EarRings />} />
-        <Route path="/bangles" element={<Bangles />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/login" element={<Login />} />
         <Route path="/Reset" element={<Reset />} />
-        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
